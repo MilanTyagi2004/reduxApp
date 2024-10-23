986ae37cfd115ff29722d8274cb9bc2f24cd114d
Milan Tyagi <milantyagi2004@gmail.com>
2024-10-23 22:26:44 +0530

    creating action-creator and reducers

--- src/component/Shop.js
+++ src/component/Shop.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Shop = () => {
   return (
     <div className='container'>
-        <h2>Buy addidas shoes at the lowest price</h2>
-      <button className='btn btn-primary'>-</button>
-      <small className='mx-2'>add item to your cart</small>
-      <button className='btn btn-primary'>+</button>
+        <h2>Your Bank Balance</h2>
+      <button className='btn btn-primary'>Withdraw Money</button>
+      <small className='mx-2'>Update Balance</small>
+      <button className='btn btn-primary'>Deposit Money</button>
     </div>
   )
 }
