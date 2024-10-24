49a63f0e0f283cfdc97e8406a398965b449f2193
Milan Tyagi <milantyagi2004@gmail.com>
2024-10-24 05:55:13 +0530

    updating state from different component

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const amount = useSelector(state => state.amount); // Using 'amount' consistently
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">Dholakpur Bank</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light"> {/* className corrected */}
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Dholakpur Bank</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/">Link</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Link</a>
                             </li>
                         </ul>
-                            <div>
-                                <button disabled={true} className='btn-primary'>Balance: 10000</button>
-                            </div>
-                       
+                        <div>
+                            <button disabled={true} className="btn btn-primary">Balance: {amount}</button> {/* Using 'amount' from state */}
+                        </div>
                     </div>
                 </div>
             </nav>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
--- src/component/Shop.js
+++ src/component/Shop.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import {useDispatch} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {Actionmakers} from '../state/index';
+
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  const {withdrawMoney,depositMoney} = bindActionCreators(Actionmakers,dispatch);
   return (
     <div className='container'>
         <h2>Your Bank Balance</h2>
-      <button className='btn btn-primary'>Withdraw Money</button>
+
+      {/*With bindActionCreator*/}
+
+      <button className='btn btn-primary' onClick={()=>{depositMoney(100)}}>Deposit Money</button>
       <small className='mx-2'>Update Balance</small>
-      <button className='btn btn-primary'>Deposit Money</button>
+      <button className='btn btn-primary' onClick={()=>{withdrawMoney(100)}}>withdrawMoney</button>
+
+
+
+      {/* Without bindActionCreator */}
+      {/* <button className='btn btn-primary' onClick={()=>{dispatch(Actionmakers.depositMoney(100))}}>Deposit Money</button>
+      <small className='mx-2'>Update Balance</small>
+      <button className='btn btn-primary' onClick={()=>{dispatch(Actionmakers.withdrawMoney(100))}}>withdrawMoney</button> */}
     </div>
   )
 }
